39fb4ef8a6446af5c2a74251a6490dccd79bf5e3
PKKostov18 <PKKostov18@codingburgas.bg>
2022-10-19 10:23:33 +0300

    Initial commit

---
+++ src/main/java/com/example/classwork10192022/Classwork10192022Application.java
@@ -0,0 +1,13 @@
+package com.example.classwork10192022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classwork10192022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classwork10192022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classwork10192022/Classwork10192022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classwork10192022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classwork10192022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
